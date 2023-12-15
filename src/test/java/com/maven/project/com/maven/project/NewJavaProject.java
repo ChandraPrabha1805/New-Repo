941758bcd573a67929de782dd33005a837cc80d5
Ankur Malviya <madanankurmalviya@gmail.com>
2023-12-15 10:52:53 -0800

    new commit2

--- src/test/java/com/maven/project/com/maven/project/NewJavaProject.java
+++ src/test/java/com/maven/project/com/maven/project/NewJavaProject.java
@@ -3,5 +3,6 @@ package com.maven.project.com.maven.project;
 public class NewJavaProject {
 public static void main(String[] args) {
 	System.out.println("My maven project");
+	System.out.println("My maven project");
 }
 }
